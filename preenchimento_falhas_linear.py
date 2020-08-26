e9a314d03d0bfbd11f351c70e7ababd33453f865
Bruno Henrique TonÃ Juliani <bruno@sim1910.simeparnet>
2020-08-26 18:02:56 -0300

    commit 26/08

--- preenchimento_falhas_linear.py
+++ preenchimento_falhas_linear.py
@@ -3,23 +3,23 @@ import numpy as np
 import os
 import datetime as dt
 
-#INPUTS
-dir_secas = "/home/bruno/Documentos/Seca_Iguacu/Dados_Estacoes"
-os.chdir(dir_secas)
+dir_seca = '/home/bruno/Documentos/Seca_Iguacu/Dados_Estacoes'
+os.chdir(dir_seca)
 
-#LISTA ESTACOES
-estacoes = ['65035000_Porto_Amazonas',
-            '65060000_Sao_Mateus_do_Sul',
-            '65100000_Rio_Negro',
-            '65155000_Sao_Bento',
-            '65208001_Pontilhao']
+estacoes = {
+            'Porto_Amazonas',
+            'Rio_Negro',
+            'Sao_Mateus_Sul',
+            'Uniao_da_Vitoria'
+             }
 
 for estacao in estacoes:
-    serie_original = pd.read_csv(estacao+'.csv', sep = ',', decimal='.')
+    serie_original = pd.read_csv(estacao+'_comparacao.csv', sep = ',', decimal='.')
     nome_estacao = estacao
-
-    serie_original.columns = ['data', 'q_m3s_orig']
-    serie_tratada = serie_original
-    serie_tratada['q_m3s_corr'] = serie_tratada.q_m3s_orig.interpolate(
-        method = 'linear', limit_direction ='forward')
-    serie_tratada.to_csv(nome_estacao+'_vazao_tratada.csv', index = False)
+    serie_original.columns = ['data', 'convencional', 'telemetrica', 'preenchimento']
+    serie_tratada = serie_original[['data', 'preenchimento']]
+    serie_tratada['q_m3s_corr'] = serie_tratada['preenchimento'].interpolate(
+        method = 'spline', order = 3, limit_direction ='forward')
+    serie_tratada.to_csv(estacao+'_preenchimento.csv', index = False)
+    #serie_tratada.to_csv(nome_estacao+'_vazao_tratada.csv', index = False)
+    
