f7f77b619aef7a88eb00334b3bc7cc702633c040
brunotjuliani <brunotjuliani@hotmail.com>
2020-08-31 17:26:04 -0300

    commit 31/08 parte 2

--- Avaliacao_modelagem.py
+++ Avaliacao_modelagem.py
@@ -15,7 +15,7 @@ dir_desempenho = "/home/bruno/Documentos/Historico_Horizontes/Desempenho"
 
 #DEFINICAO PERIODO ANALISE
 data_ini = dt.datetime(2015, 9, 3,  0,  0) #YYYY, M, D, H, Min
-data_fim = dt.datetime(2020, 8, 20,  23,  59)
+data_fim = dt.datetime(2020, 8, 30,  23,  59)
 
 
 #DEFINICAO MODELOS
@@ -23,29 +23,70 @@ modelos = ["212", "214", "216", "217"]
 horas_resumo = [1, 6] + list(np.arange(12,169,12))
 horizontes = range(168)
 
+
+
 #IMPORTA MODELO A ANALIZAR
+
+dir_aval = (dir_desempenho + '/aval_' + modelos[0][-3] + '_' +
+            data_ini.strftime("%Y%m%d") + "_" + data_fim.strftime("%Y%m%d"))
+try:
+    os.mkdir(dir_aval)
+except OSError as error:
+    pass
+nash_completo = pd.DataFrame()
+nash_resumo = pd.DataFrame()
+kge_completo = pd.DataFrame()
+kge_resumo = pd.DataFrame()
 for modelo in modelos:
     os.chdir(dir_horizontes)
     gbl["evolucao_horizontes_"+modelo] = pd.DataFrame()
     for horizonte in horizontes:
         os.chdir(dir_horizontes + "/" + modelo)
-        gbl["avaliacao_"+modelo+"_"+str(horizonte)] = pd.read_csv("Cod_"+modelo+"_Horizonte_"+str(horizonte)+".csv", header = 0)
-        gbl["avaliacao_"+modelo+"_"+str(horizonte)] = gbl["avaliacao_"+modelo+"_"+str(horizonte)].loc[gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Data"].between(str(data_ini), str(data_fim))]
-        gbl["Nash_"+modelo+"_"+str(horizonte)] = he.nse(gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Vazao_prev"],gbl["avaliacao_"+modelo+"_"+str(horizonte)]["vazao_obs"])
-        gbl["KGE_Mod_"+modelo+"_"+str(horizonte)] = he.kge_2012(gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Vazao_prev"],gbl["avaliacao_"+modelo+"_"+str(horizonte)]["vazao_obs"])
-        gbl["evolucao_horizontes_"+modelo].loc[horizonte, "Hora"] = horizonte + 1
-        gbl["evolucao_horizontes_"+modelo].loc[horizonte, "Nash"] = gbl["Nash_"+modelo+"_"+str(horizonte)]
-        gbl["evolucao_horizontes_"+modelo].loc[horizonte, "KGE_Mod"] = gbl["KGE_Mod_"+modelo+"_"+str(horizonte)]
-    os.chdir(dir_desempenho)
+        gbl["avaliacao_"+modelo+"_"+str(horizonte)] = (
+            pd.read_csv("Cod_"+modelo+"_Horizonte_"+str(horizonte)+".csv",
+                        header = 0))
+        gbl["avaliacao_"+modelo+"_"+str(horizonte)] = (
+            gbl["avaliacao_"+modelo+"_"+str(horizonte)].loc[
+                gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Data"].between(
+                    str(data_ini), str(data_fim))])
+        gbl["Nash_"+modelo+"_"+str(horizonte)] = he.nse(
+            gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Vazao_prev"],
+            gbl["avaliacao_"+modelo+"_"+str(horizonte)]["vazao_obs"])
+        gbl["KGE_Mod_"+modelo+"_"+str(horizonte)] = he.kge_2012(
+            gbl["avaliacao_"+modelo+"_"+str(horizonte)]["Vazao_prev"],
+            gbl["avaliacao_"+modelo+"_"+str(horizonte)]["vazao_obs"])
+        gbl["evolucao_horizontes_"+modelo].loc[horizonte,"Hora"] = horizonte + 1
+        gbl["evolucao_horizontes_"+modelo].loc[horizonte,"Nash"] = gbl[
+            "Nash_"+modelo+"_"+str(horizonte)]
+        gbl["evolucao_horizontes_"+modelo].loc[horizonte, "KGE_Mod"] = gbl[
+            "KGE_Mod_"+modelo+"_"+str(horizonte)]
+    os.chdir(dir_aval)
     plt.figure()
-    plt.plot(gbl["evolucao_horizontes_"+modelo].Hora, gbl["evolucao_horizontes_"+modelo].Nash, label = "Nash-Sutcliffe")
-    plt.plot(gbl["evolucao_horizontes_"+modelo].Hora, gbl["evolucao_horizontes_"+modelo].KGE_Mod, label = "KGE-2012")
+    plt.plot(gbl["evolucao_horizontes_"+modelo].Hora,
+             gbl["evolucao_horizontes_"+modelo].Nash, label = "Nash-Sutcliffe")
+    plt.plot(gbl["evolucao_horizontes_"+modelo].Hora,
+             gbl["evolucao_horizontes_"+modelo].KGE_Mod, label = "KGE-2012")
     plt.xticks(np.arange(0, max(gbl["evolucao_horizontes_"+modelo].Hora)+1, 24))
     plt.legend(loc='best')
     plt.title('Ajuste - Modelo '+modelo)
     plt.xlabel('Horizonte (h)')
     plt.ylabel('Coeficiente')
-    plt.savefig(modelo+"_Avaliacao_"+data_ini.strftime("%Y%m%d")+"_"+data_fim.strftime("%Y%m%d")+".png")
+    plt.savefig(modelo+"_Avaliacao_"+data_ini.strftime("%Y%m%d")+"_"+
+                data_fim.strftime("%Y%m%d")+".png")
     plt.close()
-    gbl["evolucao_horizontes_"+modelo].to_csv(modelo+"_Avaliacao_"+data_ini.strftime("%Y%m%d")+"_"+data_fim.strftime("%Y%m%d")+".csv")
-    gbl["evolucao_horizontes_"+modelo].loc[gbl["evolucao_horizontes_"+modelo]['Hora'].isin(horas_resumo)].to_csv(modelo+"_Resumo_"+data_ini.strftime("%Y%m%d")+"_"+data_fim.strftime("%Y%m%d")+".csv")
+    nash_completo['Nash_'+modelo] = gbl["evolucao_horizontes_"+modelo]['Nash']
+    nash_resumo['Nash_'+modelo] = gbl["evolucao_horizontes_"+modelo][
+        'Nash'].loc[gbl["evolucao_horizontes_"+modelo]['Hora'].isin(
+            horas_resumo)]
+    kge_completo['KGE_'+modelo] = gbl["evolucao_horizontes_"+modelo]['KGE_Mod']
+    kge_resumo['KGE_'+modelo] = gbl["evolucao_horizontes_"+modelo][
+        'KGE_Mod'].loc[gbl["evolucao_horizontes_"+modelo]['Hora'].isin(
+            horas_resumo)]
+nash_completo.to_csv('Nash_'+modelos[0][-3]+'_'+data_ini.strftime("%Y%m%d") +
+                     "_" + data_fim.strftime("%Y%m%d"))
+nash_resumo.to_csv('Resumo_Nash_'+modelos[0][-3]+'_'+
+                   data_ini.strftime("%Y%m%d")+"_"+data_fim.strftime("%Y%m%d"))
+kge_completo.to_csv('KGE_' + modelos[0][-3] +'_'+ data_ini.strftime("%Y%m%d") +
+                     "_" + data_fim.strftime("%Y%m%d"))
+kge_resumo.to_csv('Resumo_KGE' + modelos[0][-3]+'_'+data_ini.strftime("%Y%m%d")+
+                     "_" + data_fim.strftime("%Y%m%d"))
--- Concat_previsoes.py
+++ Concat_previsoes.py
@@ -23,25 +23,37 @@ dir_horizontes = "/home/bruno/Documentos/Historico_Horizontes"
 #DEFINICAO DOS MODELOS / ESTACOES A SEREM AVALIADOS
 
 calibracoes = {
-                #'112':'Rio_Negro','114':'Rio_Negro',
-                #'116':'Rio_Negro','117':'Rio_Negro',
-                #'212':'Porto_Amazonas','214':'Porto_Amazonas',
-                '216':'Porto_Amazonas',
-                #'217':'Porto_Amazonas',
-                #'312':'Sao_Bento','314':'Sao_Bento','316':'Sao_Bento','317':'Sao_Bento',
-                #'412':'Pontilhao','414':'Pontilhao', '416':'Pontilhao', '417':'Pontilhao',
-                #'512':'Santa_Cruz_Timbo','514':'Santa_Cruz_Timbo','516':'Santa_Cruz_Timbo','517':'Santa_Cruz_Timbo',
-                #'612':'Sao_Mateus_Sul','614':'Sao_Mateus_Sul','616':'Sao_Mateus_Sul','617':'Sao_Mateus_Sul',
-                #'712':'Divisa','714':'Divisa','716':'Divisa','717':'Divisa',
-                #'812':'Fluviopolis','814':'Fluviopolis','816':'Fluviopolis','817':'Fluviopolis',
-                #'912':'Uniao_da_Vitoria', '914':'Uniao_da_Vitoria', '916':'Uniao_da_Vitoria', '917':'Uniao_da_Vitoria',
-                #'1010':'Madereira_Gavazzoni', '1012':'Madereira_Gavazzoni', '1014':'Madereira_Gavazzoni', '1015':'Madereira_Gavazzoni',
-                #'1110':'Jangada', '1112':'Jangada', '1114':'Jangada', '1115':'Jangada',
-                #'1310':'Solais_Novo', '1312':'Solais_Novo', '1314':'Solais_Novo', '1315':'Solais_Novo',
-                #'1410':'Porto_Santo_Antonio', '1412':'Porto_Santo_Antonio', '1414':'Porto_Santo_Antonio', '1415':'Porto_Santo_Antonio',
-                #'1510':'Aguas_do_Vere', '1512':'Aguas_do_Vere', '1514':'Aguas_do_Vere', '1515':'Aguas_do_Vere',
+                '112':'Rio_Negro','114':'Rio_Negro',
+                '116':'Rio_Negro','117':'Rio_Negro',
+                '212':'Porto_Amazonas','214':'Porto_Amazonas',
+                '216':'Porto_Amazonas','217':'Porto_Amazonas',
+                #'312':'Sao_Bento','314':'Sao_Bento',
+                #'316':'Sao_Bento','317':'Sao_Bento',
+                #'412':'Pontilhao','414':'Pontilhao',
+                #'416':'Pontilhao', '417':'Pontilhao',
+                #'512':'Santa_Cruz_Timbo','514':'Santa_Cruz_Timbo',
+                #'516':'Santa_Cruz_Timbo','517':'Santa_Cruz_Timbo',
+                #'612':'Sao_Mateus_Sul','614':'Sao_Mateus_Sul',
+                #'616':'Sao_Mateus_Sul','617':'Sao_Mateus_Sul',
+                #'712':'Divisa','714':'Divisa',
+                #'716':'Divisa','717':'Divisa',
+                #'812':'Fluviopolis','814':'Fluviopolis',
+                #'816':'Fluviopolis','817':'Fluviopolis',
+                #'912':'Uniao_da_Vitoria', '914':'Uniao_da_Vitoria',
+                #'916':'Uniao_da_Vitoria', '917':'Uniao_da_Vitoria',
+                #'1010':'Madereira_Gavazzoni', '1012':'Madereira_Gavazzoni',
+                #'1014':'Madereira_Gavazzoni', '1015':'Madereira_Gavazzoni',
+                #'1110':'Jangada', '1112':'Jangada',
+                #'1114':'Jangada', '1115':'Jangada',
+                #'1310':'Solais_Novo', '1312':'Solais_Novo',
+                #'1314':'Solais_Novo', '1315':'Solais_Novo',
+                #'1410':'Porto_Santo_Antonio', '1412':'Porto_Santo_Antonio',
+                #'1414':'Porto_Santo_Antonio', '1415':'Porto_Santo_Antonio',
+                #'1510':'Aguas_do_Vere', '1512':'Aguas_do_Vere',
+                #'1514':'Aguas_do_Vere', '1515':'Aguas_do_Vere',
                 #'2010':'Porto_Capanema', '2012':'Porto_Capanema',
-                #'2110':'Hotel_Cataratas', '2112':'Hotel_Cataratas', '2114':'Hotel_Cataratas', '2115':'Hotel_Cataratas',
+                #'2110':'Hotel_Cataratas', '2112':'Hotel_Cataratas',
+                #'2114':'Hotel_Cataratas', '2115':'Hotel_Cataratas',
                 #'1212':'Foz_do_Areia',
                 #'1612':'Segredo',
                 #'1712':'Foz_do_Chopim',
@@ -57,7 +69,7 @@ for calibracao_cod, estacao_nome in calibracoes.items():
 
 #DEFINE PERIODO DE AVALIACAO
 
-data_inicio = "2020-08-16" #YYYY-MM-DD
+data_inicio = "2015-09-03" #YYYY-MM-DD
 data_fim = "2020-08-30" #YYYY-MM-DD
 periodo = pd.date_range(data_inicio, data_fim)
 periodo = pd.DataFrame(periodo)
@@ -131,38 +143,66 @@ for calibracao_cod, estacao_nome in calibracoes.items():
             #armazena serie para cada horizonte horario
             for horizonte in range(len(previsao_7d)):
                 if i == 0:
-                    gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)] = previsao_7d.iloc[[horizonte,]]
+                    gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+
+                        '_'+str(rodada)] = previsao_7d.iloc[[horizonte,]]
                 else:
                     try:
-                        gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)]
+                        gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                            str(horizonte)+'_'+str(rodada)]
                     except KeyError:
-                        gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)] = pd.DataFrame()
-                    gbl['c'+str(calibracao_cod)+'_linha_'+str(horizonte)+'_'+str(rodada)] = previsao_7d.iloc[[horizonte,]]
-                    gbl['c'+str(calibracao_cod)+'_concatenar_'+str(horizonte)+'_'+str(rodada)] = [gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)], gbl['c'+str(calibracao_cod)+'_linha_'+str(horizonte)+'_'+str(rodada)]]
-                    gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)] = pd.concat(gbl['c'+str(calibracao_cod)+'_concatenar_'+str(horizonte)+'_'+str(rodada)])
-
+                        gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                            str(horizonte)+'_'+str(rodada)] = pd.DataFrame()
+                    gbl['c'+str(calibracao_cod)+'_linha_'+str(horizonte)+'_'+
+                        str(rodada)] = previsao_7d.iloc[[horizonte,]]
+                    gbl['c'+str(calibracao_cod)+'_concatenar_'+str(horizonte)+
+                        '_'+str(rodada)] = [gbl['c'+str(calibracao_cod)+
+                                                '_horizonte_'+str(horizonte)+
+                                                '_'+str(rodada)],
+                                            gbl['c'+str(calibracao_cod)+
+                                                '_linha_'+str(horizonte)+'_'+
+                                                str(rodada)]]
+                    gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                        str(horizonte)+'_'+str(rodada)] = pd.concat(
+                            gbl['c'+str(calibracao_cod)+'_concatenar_'+
+                                str(horizonte)+'_'+str(rodada)])
 
     os.chdir(dir_cod)
     #exporta series historicas para cada horizonte
     for horizonte in range(len(previsao_7d)):
         for rodada in hora_rodadas:
             if rodada == hora_rodadas[0]:
-                gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] =(gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)])
+                gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+
+                    str(horizonte)] =(gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                                          str(horizonte)+'_'+str(rodada)])
             else:
-                gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = pd.concat([gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)],gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+'_'+str(rodada)]])
-        gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)].loc[(gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)]['Codigo']==int(calibracao_cod))].sort_index().dropna()
+                gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = (
+                    pd.concat([gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+
+                                   str(horizonte)],
+                               gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                                   str(horizonte)+'_'+str(rodada)]]))
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = (
+            gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].loc[(
+                gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+
+                    str(horizonte)]['Codigo']==int(calibracao_cod))].sort_index(
+                    ).dropna())
         #le arquivo antigo
-        gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)] = pd.read_csv("Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv", index_col = "Data")
+        #gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)] = pd.read_csv(
+        #    "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv",
+        #    index_col = "Data")
         #juncao com novas datas
-        gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = pd.concat([gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)], gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)]])
-        gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)][~gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)].index.duplicated(keep='first')]
+        #gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] =pd.concat(
+        #    [gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)],
+        #     gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)]])
+        #gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+        #    "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)][~gbl[
+        #        "Cod_"+str(calibracao_cod)+'_Horizonte_'+
+        #        str(horizonte)].index.duplicated(keep='first')]
         #remove linhas sem valor Observado
-        gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)].dropna()
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].dropna()
         #exporta serie final
-        gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)].to_csv("Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv")
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].to_csv(
+            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv")
 
     print("Concluido Bacia ",estacao_nome,", calibracao ", calibracao_cod)
     print(datetime.datetime.now())
-
-
-previsao_7d
