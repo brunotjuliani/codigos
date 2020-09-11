44da2d2f5d0cc50f2d5f739942ee3789e55e2973
brunotjuliani <brunotjuliani@hotmail.com>
2020-09-11 17:40:22 -0300

    Concat previsoes - insercao historico usinas

--- Concat_previsoes.py
+++ Concat_previsoes.py
@@ -16,24 +16,24 @@ dir_dados = "/discolocal/bruno/Coleta_Dados"
 dir_previsoes = "/discolocal/bruno/Coleta_Dados/Historico_Previsoes"
 dir_observado = "/discolocal/bruno/Observado"
 dir_horizontes = "/discolocal/bruno/Historico_Horizontes"
-
+dir_usinas = "/discolocal/bruno/Coleta_Dados/Dados_Usinas"
 
 
 #DEFINICAO DOS MODELOS / ESTACOES A SEREM AVALIADOS
 
 calibracoes = {
-#                '112':'Rio_Negro','114':'Rio_Negro',
-#                '116':'Rio_Negro','117':'Rio_Negro',
-#                '212':'Porto_Amazonas','214':'Porto_Amazonas',
-#                '216':'Porto_Amazonas','217':'Porto_Amazonas',
-#                '312':'Sao_Bento','314':'Sao_Bento',
-#                '316':'Sao_Bento','317':'Sao_Bento',
-#                '412':'Pontilhao','414':'Pontilhao',
-#                '416':'Pontilhao', '417':'Pontilhao',
-#                '512':'Santa_Cruz_Timbo','514':'Santa_Cruz_Timbo',
-#                '516':'Santa_Cruz_Timbo','517':'Santa_Cruz_Timbo',
-#                '612':'Sao_Mateus_Sul','614':'Sao_Mateus_Sul',
-#                '616':'Sao_Mateus_Sul','617':'Sao_Mateus_Sul',
+                '112':'Rio_Negro','114':'Rio_Negro',
+                '116':'Rio_Negro','117':'Rio_Negro',
+                '212':'Porto_Amazonas','214':'Porto_Amazonas',
+                '216':'Porto_Amazonas','217':'Porto_Amazonas',
+                '312':'Sao_Bento','314':'Sao_Bento',
+                '316':'Sao_Bento','317':'Sao_Bento',
+                '412':'Pontilhao','414':'Pontilhao',
+                '416':'Pontilhao', '417':'Pontilhao',
+                '512':'Santa_Cruz_Timbo','514':'Santa_Cruz_Timbo',
+                '516':'Santa_Cruz_Timbo','517':'Santa_Cruz_Timbo',
+                '612':'Sao_Mateus_Sul','614':'Sao_Mateus_Sul',
+                '616':'Sao_Mateus_Sul','617':'Sao_Mateus_Sul',
                 '712':'Divisa','714':'Divisa',
                 '716':'Divisa','717':'Divisa',
                 '812':'Fluviopolis','814':'Fluviopolis',
@@ -46,22 +46,25 @@ calibracoes = {
                 '1114':'Jangada', '1115':'Jangada',
                 '1310':'Solais_Novo', '1312':'Solais_Novo',
                 '1314':'Solais_Novo', '1315':'Solais_Novo',
-#                '1410':'Porto_Santo_Antonio', '1412':'Porto_Santo_Antonio',
-#                '1414':'Porto_Santo_Antonio', '1415':'Porto_Santo_Antonio',
-#                '1510':'Aguas_do_Vere', '1512':'Aguas_do_Vere',
-#                '1514':'Aguas_do_Vere', '1515':'Aguas_do_Vere',
-#                '2010':'Porto_Capanema', '2012':'Porto_Capanema',
-#                '2110':'Hotel_Cataratas', '2112':'Hotel_Cataratas',
-#                '2114':'Hotel_Cataratas', '2115':'Hotel_Cataratas',
-#                '1212':'Foz_do_Areia',
-#                '1612':'Segredo',
-#                '1712':'Foz_do_Chopim',
-#                '1812':'Santa_Clara',
-#                '1912':'Salto_Caxias',
+                '1410':'Porto_Santo_Antonio', '1412':'Porto_Santo_Antonio',
+                '1414':'Porto_Santo_Antonio', '1415':'Porto_Santo_Antonio',
+                '1510':'Aguas_do_Vere', '1512':'Aguas_do_Vere',
+                '1514':'Aguas_do_Vere', '1515':'Aguas_do_Vere',
+                '2010':'Porto_Capanema', '2012':'Porto_Capanema',
+                '2110':'Hotel_Cataratas', '2112':'Hotel_Cataratas',
+                '2114':'Hotel_Cataratas', '2115':'Hotel_Cataratas',
             }
+calibracoes_usinas ={
+                '1210':'GBM', '1212':'GBM', '1214':'GBM', '1215':'GBM',
+                '1218':'GBM', '1219':'GBM',
+                '1610':'SGD', '1612':'SGD', '1614':'SGD', '1615':'SGD',
+                '1710':'FCH', '1712':'FCH', '1714':'FCH', '1715':'FCH',
+                '1810':'SCL', '1812':'SCL',
+                '1910':'SCX', '1910':'SCX',
+}
 
 
-#LISTA MODELOS SELECIONADOS
+#LISTA MODELOS E ESTACOES SELECIONADOS
 for calibracao_cod, estacao_nome in calibracoes.items():
     print(calibracao_cod, estacao_nome)
 
@@ -69,7 +72,7 @@ for calibracao_cod, estacao_nome in calibracoes.items():
 #DEFINE PERIODO DE AVALIACAO
 
 data_inicio = "2015-09-03" #YYYY-MM-DD
-data_fim = "2020-09-08" #YYYY-MM-DD
+data_fim = "2020-09-10" #YYYY-MM-DD
 periodo = pd.date_range(data_inicio, data_fim)
 periodo = pd.DataFrame(periodo)
 periodo.columns = ["Data"]
@@ -83,7 +86,7 @@ periodo["Dia"] = pd.DatetimeIndex(periodo["Data"]).day.map("{:02}".format)
 hora_rodadas = ['04', '07', '10', '13', '16', '19', '22']
 
 
-#CONCATENACAO PREVISOES
+#CONCATENACAO PREVISOES ESTACOES FLUVIOMETRICAS
 
 for calibracao_cod, estacao_nome in calibracoes.items():
     print("Iniciando Bacia ",estacao_nome,", calibracao ", calibracao_cod)
@@ -182,6 +185,143 @@ for calibracao_cod, estacao_nome in calibracoes.items():
         vazao_observado = pd.read_csv("vazao_"+estacao_nome+".csv",
                                       index_col = 0).rename(columns = {
                                           'q_m3s':'vazao_obs'})
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].merge(
+                vazao_observado['vazao_obs'],how='left', left_index=True,
+                right_index=True)
+        #le arquivo antigo
+#        gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)] = pd.read_csv(
+#            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv",
+#            index_col = "Data")
+#        #juncao com novas datas
+#        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] =pd.concat(
+#            [gbl["Antigo_"+str(calibracao_cod)+"_h_"+str(horizonte)],
+#             gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+str(horizonte)]])
+#        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+#            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)][~gbl[
+#                "Cod_"+str(calibracao_cod)+'_Horizonte_'+
+#                str(horizonte)].index.duplicated(keep='first')]
+        #remove linhas sem valor Observado
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].dropna()
+        #exporta serie final
+        os.chdir(dir_cod)
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].to_csv(
+            "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)+".csv")
+
+    print("Concluido Bacia ",estacao_nome,", calibracao ", calibracao_cod)
+    print(datetime.datetime.now())
+
+
+#LISTA MODELOS E USINAS SELECIONADOS
+for calibracao_cod, estacao_nome in calibracoes_usinas.items():
+    print(calibracao_cod, estacao_nome)
+
+#CONCATENACAO PREVISOES USINAS
+
+for calibracao_cod, estacao_nome in calibracoes_usinas.items():
+    print("Iniciando Bacia ",estacao_nome,", calibracao ", calibracao_cod)
+    print(datetime.datetime.now())
+    #cria pasta respectivo a calibracao para resultado
+    dir_cod = dir_horizontes + "/" + str(calibracao_cod)
+    try:
+        os.mkdir(dir_cod)
+    except OSError as error:
+        pass
+    for rodada in hora_rodadas:
+        #leitura e interpretacao dos diferentes arquivos de previsao
+        for i in periodo.index:
+            ano = periodo.loc[i,"Ano"]
+            mes = periodo.loc[i, "Mes"]
+            dia = periodo.loc[i, "Dia"]
+            os.chdir(dir_previsoes)
+            try:
+                try:
+                    previsao_7d = pd.read_csv(str(ano)+"/"+str(mes)+"/"+
+                                              str(ano)+str(mes)+str(dia)+rodada+
+                                              ".txt", header = None)
+                except pd.errors.EmptyDataError:
+                    #print("Falha em ", periodo.iloc[i]["Data"])
+                    continue
+            except OSError:
+                #print("Falha em ", periodo.iloc[i]["Data"])
+                continue
+            previsao_7d["Codigo"] = previsao_7d[0].str.slice(0, 4).astype(
+                str).astype(int)
+            previsao_7d["Year"] = previsao_7d[0].str.slice(5,9)
+            previsao_7d["Month"] = previsao_7d[0].str.slice(10,12)
+            previsao_7d["Day"] = previsao_7d[0].str.slice(13,15)
+            previsao_7d["Hora"] = previsao_7d[0].str.slice(16,18).astype(
+                str).astype(int)
+            previsao_7d["Precip_prev"] = pd.to_numeric(previsao_7d[0].str.slice(
+                19,26), downcast = "float")
+            previsao_7d["Vazao_prev"] = pd.to_numeric(previsao_7d[0].str.slice(
+                27,35), downcast = "float")
+            previsao_7d["Data"] = pd.to_datetime(previsao_7d[[
+                "Year", "Month", "Day"]]) + pd.to_timedelta(
+                    previsao_7d["Hora"], unit = "h")
+            previsao_7d = previsao_7d.drop([
+                0, 'Year', 'Month', 'Day', 'Hora'], 1)
+            #limita a análise para a calibracao desejada - 168 passos de tempo
+            previsao_7d = previsao_7d.loc[(previsao_7d["Codigo"] == int(
+                calibracao_cod))].iloc[1:169,].reset_index(drop=True)
+            previsao_7d = previsao_7d.set_index('Data')
+            #armazena serie para cada horizonte horario
+            for horizonte in range(len(previsao_7d)):
+                if i == 0:
+                    gbl['c'+str(calibracao_cod)+'_horizonte_'+str(horizonte)+
+                        '_'+str(rodada)] = previsao_7d.iloc[[horizonte,]]
+                else:
+                    try:
+                        gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                            str(horizonte)+'_'+str(rodada)]
+                    except KeyError:
+                        gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                            str(horizonte)+'_'+str(rodada)] = pd.DataFrame()
+                    gbl['c'+str(calibracao_cod)+'_linha_'+str(horizonte)+'_'+
+                        str(rodada)] = previsao_7d.iloc[[horizonte,]]
+                    gbl['c'+str(calibracao_cod)+'_concatenar_'+str(horizonte)+
+                        '_'+str(rodada)] = [gbl['c'+str(calibracao_cod)+
+                                                '_horizonte_'+str(horizonte)+
+                                                '_'+str(rodada)],
+                                            gbl['c'+str(calibracao_cod)+
+                                                '_linha_'+str(horizonte)+'_'+
+                                                str(rodada)]]
+                    gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                        str(horizonte)+'_'+str(rodada)] = pd.concat(
+                            gbl['c'+str(calibracao_cod)+'_concatenar_'+
+                                str(horizonte)+'_'+str(rodada)])
+
+    #exporta series historicas para cada horizonte
+    os.chdir(dir_cod)
+    for horizonte in range(len(previsao_7d)):
+        for rodada in hora_rodadas:
+            if rodada == hora_rodadas[0]:
+                gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+
+                    str(horizonte)] =(gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                                          str(horizonte)+'_'+str(rodada)])
+            else:
+                gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = (
+                    pd.concat([gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+
+                                   str(horizonte)],
+                               gbl['c'+str(calibracao_cod)+'_horizonte_'+
+                                   str(horizonte)+'_'+str(rodada)]]))
+        gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = (
+            gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].loc[(
+                gbl["Cod_" + str(calibracao_cod)+'_Horizonte_'+
+                    str(horizonte)]['Codigo']==int(calibracao_cod))].sort_index(
+                    ).dropna())
+        #importa e junta observado e previsao
+        os.chdir(dir_usinas)
+        vazao_observado = pd.read_csv(estacao_nome+".txt", header = None, skiprows = 1)
+        vazao_observado['Year'] = vazao_observado[0].str.slice(0,4)
+        vazao_observado['Month'] = vazao_observado[0].str.slice(5,7)
+        vazao_observado['Day'] = vazao_observado[0].str.slice(8,10)
+        vazao_observado['Hora'] = vazao_observado[0].str.slice(11,13).astype(str).astype(int)
+        vazao_observado['vazao_obs'] = pd.to_numeric(vazao_observado[0].str.slice(14,22), errors = 'coerce')
+        vazao_observado['Data'] = pd.to_datetime(vazao_observado[['Year', 'Month', 'Day']]) + pd.to_timedelta(vazao_observado['Hora'], unit = 'h')
+        vazao_observado = vazao_observado.set_index('Data')
+        vazao_observado = vazao_observado.drop([0, 'Year', 'Month', 'Day', 'Hora'], 1)
         gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
             "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].merge(
                 vazao_observado['vazao_obs'],how='left', left_index=True,
--- teste.py
+++ teste.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
+import requests
 import pytz
 gbl = globals()
 
@@ -13,8 +14,10 @@ gbl = globals()
 data_ini = dt.datetime(2019, 6, 1,  0,  0) #YYYY, M, D, H, Min
 data_fim = dt.datetime(2019, 6, 15,  23,  59)
 
+dir_usinas = "/discolocal/bruno/Coleta_Dados/Dados_Usinas"
 dir_observado = "/discolocal/bruno/Observado"
 os.chdir(dir_observado)
+
 
 nome_bacia = 'Santa_Cruz_Timbo'
 
@@ -41,3 +44,31 @@ plt.annotate(data_texto, xy=(1,1), xytext=(-4,26), fontsize=10,
              xycoords='axes fraction', textcoords='offset points',
              bbox=dict(facecolor='white', alpha=0.8),
              horizontalalignment='right', verticalalignment='top')
+
+
+os.chdir(dir_observado)
+estacao_nome = 'Santa_Cruz_Timbo'
+vazao_observado = pd.read_csv("vazao_"+estacao_nome+".csv",
+                              index_col = 0).rename(columns = {
+                                  'q_m3s':'vazao_obs'})
+vazao_observado
+
+gbl["Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)] = gbl[
+    "Cod_"+str(calibracao_cod)+'_Horizonte_'+str(horizonte)].merge(
+        vazao_observado['vazao_obs'],how='left', left_index=True,
+        right_index=True)
+
+os.chdir(dir_usinas)
+estacao_nome = 'DRJ'
+vazao_observado = pd.read_csv(estacao_nome+".txt", header = None, skiprows = 1)
+vazao_observado['Year'] = vazao_observado[0].str.slice(0,4)
+vazao_observado['Month'] = vazao_observado[0].str.slice(5,7)
+vazao_observado['Day'] = vazao_observado[0].str.slice(8,10)
+vazao_observado['Hora'] = vazao_observado[0].str.slice(11,13).astype(str).astype(int)
+vazao_observado['vazao_obs'] = pd.to_numeric(vazao_observado[0].str.slice(14,22), errors = 'coerce')
+vazao_observado['Data'] = pd.to_datetime(vazao_observado[['Year', 'Month', 'Day']]) + pd.to_timedelta(vazao_observado['Hora'], unit = 'h')
+vazao_observado = vazao_observado.set_index('Data')
+vazao_observado = vazao_observado.drop([0, 'Year', 'Month', 'Day', 'Hora'], 1)
+
+vazao_observado
+vazao_observado
