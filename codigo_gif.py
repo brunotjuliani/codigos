040154b800cb7cfd0b679f21d2776d2113fefa15
brunotjuliani <brunotjuliani@hotmail.com>
2021-07-05 09:37:17 -0300

    commit 05-07

--- codigo_gif.py
+++ codigo_gif.py
@@ -6,7 +6,7 @@ import os
 
 #DEFINE VARIAVEIS
 modelo = "ECMWF" #COPEL / ECMWF / NCEP_WRF5K
-data = "2021-06-21"
+data = "2021-07-05"
 diretorio = "/discolocal/bruno/Mapas_Chuva/"+modelo+"/"+data
 #diretorio = "/Users/brunojuliani/github/dados/"+modelo+"/"+data
 
